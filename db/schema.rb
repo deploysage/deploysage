25400cccc8e7b465b598953fb7569b0336f379d5
Pivotal Tracker <labs-tracker-team@pivotal.io>
2016-01-13 01:29:33 -0700

    generated schema change

--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(version: 20160103025638) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "uid",        null: false
     t.string   "handle",     null: false
+    t.string   "uid",        null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["uid"], name: "index_users_on_uid", using: :btree
