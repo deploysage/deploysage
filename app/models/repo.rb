1505bea01f14c40a9f9b3c574b6840f1c9689422
Chad Woolley <thewoolleyman@gmail.com>
2016-08-23 10:20:29 -0700

    [#114147735] remove unused StateBroadcastJob

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -2,8 +2,4 @@
 class Repo < ActiveRecord::Base
   # belongs_to :org # disabled until we can scope Repo finds to user to avoid Brakeman warnings
   validates :url, :github_identifier, presence: true
-
-  # after_update_commit do
-  #   StateBroadcastJob.perform_later(self)
-  # end
 end
